ea7207dc2defb2ce5fc936bc9d5b95f8c7efa1e2
VermouthX <vermouth7dante@gmail.com>
2020-07-22 17:02:09 +0800

    :tada: Startup project

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.intellij.tasks.PatchPluginXmlTask
+
+plugins {
+    id("org.jetbrains.intellij") version "0.4.21"
+    java
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "com.calvincode"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("junit", "junit", "4.12")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "2020.1.3"
+}
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+tasks.getByName<PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+      Add change notes here.<br>
+      <em>most HTML tags may be used</em>""")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "intellij-investor-dashboard"
+
