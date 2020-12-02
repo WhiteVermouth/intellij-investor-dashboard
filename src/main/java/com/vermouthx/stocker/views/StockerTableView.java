24450bfeb1ec7b5da9227482f5346616af8eb40e
VermouthX <vermouth7dante@gmail.com>
2020-12-02 21:22:47 +0800

    :zap: Enhance table view

--- src/main/java/com/vermouthx/stocker/views/StockerTableView.java
+++ src/main/java/com/vermouthx/stocker/views/StockerTableView.java
@@ -1,6 +1,11 @@
 package com.vermouthx.stocker.views;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.ui.JBColor;
+import com.intellij.ui.components.JBLabel;
+import com.intellij.ui.components.JBPanel;
+import com.intellij.ui.components.JBScrollPane;
+import com.intellij.ui.table.JBTable;
 import com.vermouthx.stocker.enums.StockerQuoteColorPattern;
 import com.vermouthx.stocker.settings.StockerSetting;
 
@@ -12,6 +17,8 @@ import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 
 public class StockerTableView {
+    private Logger log = Logger.getInstance(getClass());
+
     private JPanel mPane;
     private JScrollPane tbPane;
     private JPanel dtPane;
@@ -28,16 +35,24 @@ public class StockerTableView {
     }
 
     private void initPane() {
+        mPane = new JBPanel<>(new BorderLayout());
+        tbPane = new JBScrollPane();
+        dtPane = new JBPanel<>(new FlowLayout());
         tbPane.setBorder(BorderFactory.createEmptyBorder());
-        dtPane.setBorder(BorderFactory.createEmptyBorder(1, 10, 10, 10));
+        dtPane.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, JBColor.border()));
+        mPane.add(tbPane, BorderLayout.CENTER);
+        mPane.add(dtPane, BorderLayout.SOUTH);
     }
 
     private void initTable() {
-        tbModel = new StockerTableModel();
         String codeColumn = "Code";
         String nameColumn = "Name";
         String currentColumn = "Current";
         String percentColumn = "Percentage";
+        tbView = new JBTable();
+        tbModel = new StockerTableModel();
+        tbPane.add(tbView);
+        tbPane.setViewportView(tbView);
         tbModel.setColumnIdentifiers(new String[]{codeColumn, nameColumn, currentColumn, percentColumn});
         tbView.addFocusListener(new FocusAdapter() {
             @Override
@@ -46,6 +61,14 @@ public class StockerTableView {
             }
         });
         tbView.setModel(tbModel);
+        tbView.getTableHeader().setReorderingAllowed(false);
+        tbView.getTableHeader().setDefaultRenderer(new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+                setHorizontalAlignment(DefaultTableCellRenderer.CENTER);
+                return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+            }
+        });
         tbView.getColumn(codeColumn).setCellRenderer(new DefaultTableCellRenderer() {
             @Override
             public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
@@ -65,7 +88,7 @@ public class StockerTableView {
             public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
                 updateColorPattern();
                 setHorizontalAlignment(DefaultTableCellRenderer.CENTER);
-                String v = table.getValueAt(row, column + 1).toString();
+                String v = table.getValueAt(row, table.getColumn(percentColumn).getModelIndex()).toString();
                 applyColorPattern(v, this);
                 return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
             }
@@ -85,22 +108,26 @@ public class StockerTableView {
     private void applyColorPattern(String value, DefaultTableCellRenderer renderer) {
         String grayColorHex = "#888888";
         if (value.startsWith("+")) {
-            renderer.setForeground(upColor);
+            renderer.setBackground(upColor);
         } else if (value.startsWith("-")) {
-            renderer.setForeground(downColor);
+            renderer.setBackground(downColor);
         } else {
-            renderer.setForeground(JBColor.decode(grayColorHex));
+            renderer.setBackground(JBColor.decode(grayColorHex));
         }
+        renderer.setForeground(JBColor.decode("#F8F8F2"));
     }
 
     private void initDatetimeLabel() {
+        lbDatetimeContent = new JBLabel();
         String datetimeColorHex = "#6F84CA";
         lbDatetimeContent.setForeground(JBColor.decode(datetimeColorHex));
+        dtPane.add(new JBLabel("Last update at: "));
+        dtPane.add(lbDatetimeContent);
     }
 
     private void updateColorPattern() {
-        String redColorHex = "#F44244";
-        String greenColorHex = "#0EBB70";
+        String redColorHex = "#E93F41";
+        String greenColorHex = "#0DB06A";
         StockerSetting setting = StockerSetting.Companion.getInstance();
         if (setting.getQuoteColorPattern() == StockerQuoteColorPattern.RED_UP_GREEN_DOWN) {
             upColor = JBColor.decode(redColorHex);
