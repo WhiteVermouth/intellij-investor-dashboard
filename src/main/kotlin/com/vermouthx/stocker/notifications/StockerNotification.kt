d756c8ce87d81d4543ddf6c546f0b7276ff13c39
VermouthX <vermouth7dante@gmail.com>
2021-01-23 16:42:27 +0800

    ♻️ Notification code refactor

--- src/main/kotlin/com/vermouthx/stocker/notifications/StockerNotification.kt
+++ src/main/kotlin/com/vermouthx/stocker/notifications/StockerNotification.kt
@@ -6,8 +6,26 @@ import com.intellij.notification.NotificationListener
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.IconLoader
+import org.intellij.lang.annotations.Language
 
 object StockerNotification {
+    @Language("HTML")
+    private val whatsNew: String = """
+        <li>Bug fix</li>
+    """.trimIndent()
+
+    @Language("HTML")
+    private val releaseNote: String = """
+        <div>
+            <h3>What's new</h3>
+            <ul>
+                $whatsNew
+            </ul>
+            <p>Please visit the <a href="https://github.com/WhiteVermouth/intellij-investor-dashboard/blob/master/CHANGELOG.md">Changelog</a> for more details.</p>>
+            <p>Thank you for choosing Stocker.</p>
+        </div>
+    """.trimIndent()
+
     private val notificationGroup = NotificationGroup(
         displayId = "Stocker",
         displayType = NotificationDisplayType.STICKY_BALLOON,
@@ -17,16 +35,9 @@ object StockerNotification {
     @JvmField
     var logoIcon = IconLoader.getIcon("/icons/logo.svg", javaClass)
 
-    private val releaseNote: String = """
-        What's new?<br>
-            - Bug fix<br>
-        Please visit the <a href="https://github.com/WhiteVermouth/intellij-investor-dashboard/blob/master/CHANGELOG.md">Changelog</a> for more details.<br>
-        Thank you for choosing Stocker.
-    """.trimIndent()
-
     fun notifyReleaseNote(project: Project, version: String) {
         notificationGroup.createNotification(
-            title = "Stocker Updated to $version",
+            title = "Stocker Updated to v$version",
             content = releaseNote,
             type = NotificationType.INFORMATION,
             listener = NotificationListener.URL_OPENING_LISTENER
