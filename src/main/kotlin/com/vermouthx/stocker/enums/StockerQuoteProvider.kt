eb010e001dcbcdedd017b02aecfab050ea960559
VermouthX <zihan.ma@protonmail.com>
2021-04-10 23:40:13 +0800

    ⚡ Use https when refreshing

--- src/main/kotlin/com/vermouthx/stocker/enums/StockerQuoteProvider.kt
+++ src/main/kotlin/com/vermouthx/stocker/enums/StockerQuoteProvider.kt
@@ -7,8 +7,8 @@ enum class StockerQuoteProvider(
     val providerPrefixMap: Map<StockerMarketType, String>
 ) {
     SINA(
-        title = "Sina", host = "http://hq.sinajs.cn/list=",
-        suggestHost = "http://suggest3.sinajs.cn/suggest/key=",
+        title = "Sina", host = "https://hq.sinajs.cn/list=",
+        suggestHost = "https://suggest3.sinajs.cn/suggest/key=",
         providerPrefixMap = mapOf(
             StockerMarketType.AShare to "",
             StockerMarketType.HKStocks to "hk",
@@ -16,13 +16,19 @@ enum class StockerQuoteProvider(
             StockerMarketType.Crypto to "btc_"
         )
     ),
+
+    /**
+     * Tencent API is deprecated
+     * Keep this item only for compatibility concern
+     */
     TENCENT(
-        title = "Tencent", host = "http://qt.gtimg.cn/q=",
+        title = "Tencent", host = "https://qt.gtimg.cn/q=",
         suggestHost = "https://smartbox.gtimg.cn/s3/?v=2&t=all&c=1&q=",
         providerPrefixMap = mapOf(
             StockerMarketType.AShare to "",
             StockerMarketType.HKStocks to "hk",
-            StockerMarketType.USStocks to "us"
+            StockerMarketType.USStocks to "us",
+            StockerMarketType.Crypto to "btc_"
         )
     )
 }
