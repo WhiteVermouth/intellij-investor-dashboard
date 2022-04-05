bbef0b644480117ad50bb7d573b2b27accbe7f2b
Zihan Ma <zihan.ma@protonmail.com>
2022-04-06 01:15:24 +0800

    ✨ New management dialog

--- src/main/kotlin/com/vermouthx/stocker/views/dialogs/StockerManagementDialog.kt
+++ src/main/kotlin/com/vermouthx/stocker/views/dialogs/StockerManagementDialog.kt
@@ -3,26 +3,32 @@ package com.vermouthx.stocker.views.dialogs
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.ui.JBColor
+import com.intellij.ui.CollectionListModel
+import com.intellij.ui.ToolbarDecorator
+import com.intellij.ui.components.JBList
 import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.components.JBTabbedPane
 import com.intellij.ui.layout.CCFlags
 import com.intellij.ui.layout.panel
 import com.vermouthx.stocker.StockerAppManager
 import com.vermouthx.stocker.entities.StockerQuote
-import com.vermouthx.stocker.entities.StockerSuggestion
 import com.vermouthx.stocker.enums.StockerMarketType
-import com.vermouthx.stocker.enums.StockerStockOperation
 import com.vermouthx.stocker.settings.StockerSetting
-import com.vermouthx.stocker.utils.StockerActionUtil
 import com.vermouthx.stocker.utils.StockerQuoteHttpUtil
-import javax.swing.*
+import java.awt.BorderLayout
+import java.awt.event.ActionEvent
+import javax.swing.Action
+import javax.swing.BorderFactory
+import javax.swing.JComponent
+import javax.swing.JPanel
 
 class StockerManagementDialog(val project: Project?) : DialogWrapper(project) {
 
     private val setting = StockerSetting.instance
 
-    private val tabMap: MutableMap<Int, JBScrollPane> = mutableMapOf()
+    private val tabMap: MutableMap<Int, JPanel> = mutableMapOf()
+
+    private val currentSymbols: MutableMap<StockerMarketType, CollectionListModel<StockerQuote>> = mutableMapOf()
 
     private var currentMarketSelection: StockerMarketType = StockerMarketType.AShare
 
@@ -38,136 +44,124 @@ class StockerManagementDialog(val project: Project?) : DialogWrapper(project) {
         tabbedPane.add("US", createTabContent(2))
 //        tabbedPane.add("Crypto", createTabContent(3))
         tabbedPane.addChangeListener {
-            val setting = StockerSetting.instance
-            when (tabbedPane.selectedIndex) {
+            currentMarketSelection = when (tabbedPane.selectedIndex) {
                 0 -> {
-                    currentMarketSelection = StockerMarketType.AShare
-                    val quotes = StockerQuoteHttpUtil.get(
-                        StockerMarketType.AShare,
-                        setting.quoteProvider,
-                        setting.aShareList
-                    )
-                    tabMap[tabbedPane.selectedIndex]?.let { sp -> renderTabPane(sp, quotes) }
+                    StockerMarketType.AShare
                 }
                 1 -> {
-                    currentMarketSelection = StockerMarketType.HKStocks
-                    val quotes = StockerQuoteHttpUtil.get(
-                        StockerMarketType.HKStocks,
-                        setting.quoteProvider,
-                        setting.hkStocksList
-                    )
-                    tabMap[tabbedPane.selectedIndex]?.let { sp -> renderTabPane(sp, quotes) }
+                    StockerMarketType.HKStocks
                 }
                 2 -> {
-                    currentMarketSelection = StockerMarketType.USStocks
-                    val quotes = StockerQuoteHttpUtil.get(
-                        StockerMarketType.USStocks,
-                        setting.quoteProvider,
-                        setting.usStocksList
-                    )
-                    tabMap[tabbedPane.selectedIndex]?.let { sp -> renderTabPane(sp, quotes) }
+                    StockerMarketType.USStocks
                 }
-                3 -> {
-                    currentMarketSelection = StockerMarketType.Crypto
-                    val quotes = StockerQuoteHttpUtil.get(
-                        StockerMarketType.Crypto,
-                        setting.quoteProvider,
-                        setting.cryptoList
-                    )
-                    tabMap[tabbedPane.selectedIndex]?.let { sp -> renderTabPane(sp, quotes) }
-                }
+//                3 -> {
+//                    StockerMarketType.Crypto
+//                }
                 else -> return@addChangeListener
             }
         }
-        tabMap[0]?.let { sp ->
-            val setting = StockerSetting.instance
-            renderTabPane(
-                sp,
-                StockerQuoteHttpUtil.get(StockerMarketType.AShare, setting.quoteProvider, setting.aShareList)
+
+        val aShareListModel = CollectionListModel(
+            StockerQuoteHttpUtil.get(
+                StockerMarketType.AShare,
+                setting.quoteProvider,
+                setting.aShareList
             )
+        )
+        currentSymbols[StockerMarketType.AShare] = aShareListModel
+        tabMap[0]?.let { pane ->
+            renderTabPane(pane, aShareListModel)
         }
 
+        val hkStocksListModel = CollectionListModel(
+            StockerQuoteHttpUtil.get(
+                StockerMarketType.HKStocks,
+                setting.quoteProvider,
+                setting.hkStocksList
+            )
+        )
+        currentSymbols[StockerMarketType.HKStocks] = hkStocksListModel
+        tabMap[1]?.let { pane ->
+            renderTabPane(pane, hkStocksListModel)
+        }
+
+        val usStocksListModel = CollectionListModel(
+            StockerQuoteHttpUtil.get(
+                StockerMarketType.USStocks,
+                setting.quoteProvider,
+                setting.usStocksList
+            )
+        )
+        currentSymbols[StockerMarketType.USStocks] = usStocksListModel
+        tabMap[2]?.let { pane ->
+            renderTabPane(pane, usStocksListModel)
+        }
+
+        tabbedPane.selectedIndex = 0
         return panel {
             row {
                 tabbedPane(CCFlags.grow)
             }
-        }.withPreferredWidth(400).withPreferredHeight(500)
+        }.withPreferredWidth(300).withPreferredHeight(500)
     }
 
     override fun createActions(): Array<Action> {
-        return emptyArray()
+        return arrayOf(
+            object : OkAction() {
+                override fun actionPerformed(e: ActionEvent?) {
+                    val myApplication = StockerAppManager.myApplication(project)
+                    if (myApplication != null) {
+                        myApplication.shutdownThenClear()
+                        currentSymbols[StockerMarketType.AShare]?.let { symbols ->
+                            setting.aShareList = symbols.items.map { it.code }.toMutableList()
+                        }
+                        currentSymbols[StockerMarketType.HKStocks]?.let { symbols ->
+                            setting.hkStocksList = symbols.items.map { it.code }.toMutableList()
+                        }
+                        currentSymbols[StockerMarketType.USStocks]?.let { symbols ->
+                            setting.usStocksList = symbols.items.map { it.code }.toMutableList()
+                        }
+                        myApplication.schedule()
+                    }
+                    super.actionPerformed(e)
+                }
+            },
+            cancelAction
+        )
     }
 
     private fun createTabContent(index: Int): JComponent {
-        val scrollPane = JBScrollPane()
-        tabMap[index] = scrollPane
+        val pane = JPanel(BorderLayout())
+        tabMap[index] = pane
         return panel {
             row {
-                scrollPane(scrollPane)
+                pane(CCFlags.grow)
             }
         }.withPreferredHeight(500)
     }
 
-    private fun renderTabPane(scrollPane: JBScrollPane, symbols: List<StockerQuote>) {
-        scrollPane.setViewportView(
+    private fun renderTabPane(pane: JPanel, listModel: CollectionListModel<StockerQuote>) {
+        val list = JBList(listModel)
+        val toolbar = ToolbarDecorator.createDecorator(list)
+        val toolbarPane = toolbar.createPanel()
+        list.installCellRenderer { symbol ->
             panel {
-                symbols.forEach { symbol ->
-                    val actionButton = JButton(StockerStockOperation.STOCK_DELETE.operation)
-                    val separator = JSeparator().also {
-                        it.foreground = JBColor.border()
-                        it.background = JBColor.background()
-                    }
-                    row {
-                        label(symbol.code)
-                        label(
-                            if (symbol.name.length <= 20) {
-                                symbol.name
-                            } else {
-                                "${symbol.name.substring(0, 20)}..."
-                            }
-                        )
-                        actionButton.addActionListener {
-                            val myApplication = StockerAppManager.myApplication(project)
-                            if (myApplication != null) {
-                                myApplication.shutdownThenClear()
-                                when (StockerStockOperation.mapOf(actionButton.text)) {
-                                    StockerStockOperation.STOCK_ADD -> {
-                                        if (StockerActionUtil.addStock(
-                                                currentMarketSelection,
-                                                StockerSuggestion(symbol.code, symbol.name, currentMarketSelection),
-                                                project
-                                            )
-                                        ) {
-                                            actionButton.text = StockerStockOperation.STOCK_DELETE.operation
-                                        }
-                                    }
-                                    StockerStockOperation.STOCK_DELETE -> {
-                                        if (StockerActionUtil.removeStock(
-                                                currentMarketSelection,
-                                                StockerSuggestion(symbol.code, symbol.name, currentMarketSelection)
-                                            )
-                                        ) {
-                                            actionButton.text = StockerStockOperation.STOCK_ADD.operation
-                                        }
-                                    }
-                                    else -> {
-                                        myApplication.schedule()
-                                        return@addActionListener
-                                    }
-                                }
-                                myApplication.schedule()
-                            }
+                row {
+                    label(symbol.code)
+                    label(
+                        if (symbol.name.length <= 20) {
+                            symbol.name
+                        } else {
+                            "${symbol.name.substring(0, 20)}..."
                         }
-                        right {
-                            actionButton()
-                        }
-                    }
-                    row {
-                        separator(CCFlags.growX)
-                    }
+                    )
                 }
-            }.withBorder(BorderFactory.createEmptyBorder(16, 16, 16, 16))
-        )
+            }.withBorder(BorderFactory.createEmptyBorder(8, 16, 8, 16))
+        }
+        val scrollPane = JBScrollPane()
+        scrollPane.setViewportView(list)
+        pane.add(toolbarPane, BorderLayout.NORTH)
+        pane.add(scrollPane, BorderLayout.CENTER)
     }
 
 }
