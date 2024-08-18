64c86047c17b0d0df2b75b51ffe9a0c882ef451b
Zihan Ma <zihan.ma@protonmail.com>
2024-08-19 00:55:43 +0800

    ⬆️ Upgrade intellij-platform-gradle-plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,15 @@
 import org.jetbrains.changelog.Changelog
-import org.jetbrains.intellij.tasks.RunPluginVerifierTask.FailureLevel
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
+import org.jetbrains.intellij.platform.gradle.models.ProductRelease
+import org.jetbrains.intellij.platform.gradle.tasks.VerifyPluginTask
 
 fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
-    // Java support
     id("java")
-    // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.8.21"
-    // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.15.0"
-    // Gradle Changelog Plugin
-    id("org.jetbrains.changelog") version "2.0.0"
+    id("org.jetbrains.kotlin.jvm") version "2.0.10"
+    id("org.jetbrains.intellij.platform") version "2.0.1"
+    id("org.jetbrains.changelog") version "2.2.1"
 }
 
 group = properties("pluginGroup")
@@ -19,20 +17,17 @@ version = properties("pluginVersion")
 
 repositories {
     mavenCentral()
-}
-
-kotlin {
-    jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+    intellijPlatform {
+        defaultRepositories()
     }
 }
 
-intellij {
-    pluginName.set(properties("pluginName"))
-    version.set(properties("platformVersion"))
-    type.set(properties("platformType"))
-
-    updateSinceUntilBuild.set(false)
+dependencies {
+    intellijPlatform {
+        create(properties("platformType"), properties("platformVersion"))
+        pluginVerifier()
+        instrumentationTools()
+    }
 }
 
 changelog {
@@ -41,46 +36,52 @@ changelog {
     groups.set(emptyList())
 }
 
-tasks {
-    buildSearchableOptions {
-        enabled = false
-    }
-    patchPluginXml {
-        version.set(properties("pluginVersion"))
+val pluginDescription = """
+    <div>
+      <p>
+        Stocker is a JetBrains IDE extension dashboard for investors to track
+        realtime stock market conditions.
+      </p>
+      <h2>Tutorial</h2>
+      <p>
+        All instructions can be found at
+        <a href="https://nszihan.com/2021/04/11/stocker">here</a>.
+      </p>
+      <h2>Licence</h2>
+      <a href="https://raw.githubusercontent.com/WhiteVermouth/intellij-investor-dashboard/master/LICENSE">Apache 2.0 License</a>
+      <h2>Donation</h2>
+      <p>If you like this plugin, you can <a href="https://www.buymeacoffee.com/nszihan">buy me a cup of coffee</a>. Thank you!</p>
+    </div>
+""".trimIndent()
 
-        val description = """
-            <div>
-              <p>
-                Stocker is a JetBrains IDE extension dashboard for investors to track
-                realtime stock market conditions.
-              </p>
-              <h2>Tutorial</h2>
-              <p>
-                All instructions can be found at
-                <a href="https://nszihan.com/2021/04/11/stocker">here</a>.
-              </p>
-              <h2>Licence</h2>
-              <a href="https://raw.githubusercontent.com/WhiteVermouth/intellij-investor-dashboard/master/LICENSE">Apache 2.0 License</a>
-              <h2>Donation</h2>
-              <p>If you like this plugin, you can <a href="https://www.buymeacoffee.com/nszihan">buy me a cup of coffee</a>. Thank you!</p>
-            </div>
-        """.trimIndent()
-
-        pluginDescription.set(description)
-        changeNotes.set(provider { changelog.renderItem(changelog.getLatest(), Changelog.OutputType.HTML) })
+intellijPlatform {
+    buildSearchableOptions = false
+    pluginConfiguration {
+        name = properties("pluginName")
+        version = properties("pluginVersion")
+        description = pluginDescription
+        changeNotes = provider {
+            changelog.renderItem(changelog.getLatest(), Changelog.OutputType.HTML)
+        }
+        ideaVersion {
+            untilBuild = provider { null }
+        }
     }
-    runPluginVerifier {
-        ideVersions.set(
-            properties("pluginVerifierIdeVersions").split(",").map(String::trim).filter(String::isNotEmpty)
-        )
-        failureLevel.set(
-            listOf(
-                FailureLevel.COMPATIBILITY_PROBLEMS, FailureLevel.INVALID_PLUGIN
-            )
-        )
+    publishing {
+        token = System.getProperty("jetbrains.token")
     }
-    publishPlugin {
-        dependsOn("patchChangelog")
-        token.set(System.getProperty("jetbrains.token"))
+    pluginVerification {
+        ides {
+            recommended()
+            select {
+                types = listOf(IntelliJPlatformType.IntellijIdeaCommunity, IntelliJPlatformType.IntellijIdeaUltimate)
+                channels = listOf(ProductRelease.Channel.RELEASE)
+                sinceBuild = "233"
+                untilBuild = "242.*"
+            }
+        }
+        failureLevel = listOf(
+            VerifyPluginTask.FailureLevel.COMPATIBILITY_PROBLEMS, VerifyPluginTask.FailureLevel.INVALID_PLUGIN
+        )
     }
 }
